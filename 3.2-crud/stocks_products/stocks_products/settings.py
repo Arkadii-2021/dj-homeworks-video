88024650e106c52a759a153f03b1c8e0d9cf0afa
Arkadii Kozhuhow <Arkadii_kozhuhow@mail.ru>
2022-02-21 11:05:06 +0700

    add drf_intro_fix_product_search -hw

--- 3.2-crud/stocks_products/stocks_products/settings.py
+++ 3.2-crud/stocks_products/stocks_products/settings.py
@@ -132,6 +132,6 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'SEARCH_PARAM': 'product',
+    'SEARCH_PARAM': 'search',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
 }
