0c27b44dab5738b28dc22c4fa9eb02a8d5283270
Arkadii Kozhuhow <arkadii_kozhuhow@mail.ru>
2022-02-26 12:51:56 +0700

    add permissions_in_drf -hw

--- 3.3-permissions/api_with_restrictions/advertisements/serializers.py
+++ 3.3-permissions/api_with_restrictions/advertisements/serializers.py
@@ -41,8 +41,8 @@ class AdvertisementSerializer(serializers.ModelSerializer):
         """Метод для валидации. Вызывается при создании и обновлении."""
         adv_set_status = Advertisement.objects.filter(status='OPEN')
         adv_creator = self.context['request'].user
-        partial_update = self.context['view'].action == 'partial_update'
-        create = self.context['view'].action == 'create'
+        partial_update = self.context['view'].action = 'partial_update'
+        create = self.context['view'].action = 'create'
 
         if len(adv_set_status.filter(creator=adv_creator, status='OPEN')) >= 10 and (create or not partial_update):
             raise ValidationError('Превышен лимит в 10 объявлений')
