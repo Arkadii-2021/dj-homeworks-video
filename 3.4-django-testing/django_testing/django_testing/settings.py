ffe49cb42cf8f75a7be52cd0e163b342ca3d5e09
Arkadii Kozhuhow <arkadii_kozhuhow@mail.ru>
2022-03-02 22:12:41 +0700

    add django-testing -hw

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -77,12 +77,11 @@ WSGI_APPLICATION = 'django_testing.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'netology_django_testing',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -114,8 +113,6 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
-USE_L10N = True
 
 USE_TZ = True
 
